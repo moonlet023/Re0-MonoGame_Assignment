193709352fd3fc858cb7cc68e320f7a3251a58f8
moonlet023 <moonlet023@gmail.com>
2023-11-20 20:21:47 +0800

    I don't know what 7 I doing 3

--- Re0-MonoGame_Assignment/Re0-MonoGame_Assignment/Game1.cs
+++ Re0-MonoGame_Assignment/Re0-MonoGame_Assignment/Game1.cs
@@ -141,10 +141,11 @@ namespace Re0_MonoGame_Assignment
                     {
                         apple[i] = new Apple(this);
                         this.Components.Add(apple[i]);
-                        
-                        Matrix appleTransform = Matrix.CreateTranslation(new Vector3(-apple[i].center, 0.0f)) *
-                                               Matrix.CreateRotationZ(apple[i].rotationAngle) *
-                                               Matrix.CreateTranslation(new Vector3(apple[i].position, 0.0f));
+
+                        if (IntersectsPixel(apple[i].appleRect, apple[i].data, gamePlate.platrRect, gamePlate.data))
+                        {
+                            apple[i].isHit = true;
+                        }
                     }
                     time = basicTime + (basicTime * (stage - 1));
                 }
@@ -193,32 +194,30 @@ namespace Re0_MonoGame_Assignment
             base.Update(gameTime);
         }
 
-        private static bool PixelCollision(Matrix transformA, Rectangle rectA, int widthA, int heightA, ref Color[] dataA, Matrix transformB, int widthB, int heightB,ref Color[] dataB)
+        public static Rectangle CalculateBoundingRectangle(Rectangle rectangle, Matrix transform)
         {
-            Matrix AToB = transformA * Matrix.Invert(transformB);
 
-            Vector2 stepX = Vector2.TransformNormal(Vector2.UnitX, AToB);
-            Vector2 stepY = Vector2.TransformNormal(Vector2.UnitY, AToB);
-            Vector2 yPosInB = Vector2.Transform(Vector2.Zero, AToB);
-            for (int yA = rectA.Top; yA < rectA.Bottom; yA++)
-            {   // For each row in A
-                Vector2 posInB = yPosInB; // At the beginning of the row
-                for (int xA = rectA.Left; xA < rectA.Right; xA++)
-                { // For each pixel in the row
-                    int xB = (int)Math.Round(posInB.X); int yB = (int)Math.Round(posInB.Y);
-                    if (0 <= xB && xB < widthB && 0 <= yB && yB < heightB)
-                    {
-                        Color colorA = dataA[xA + yA * widthA]; Color colorB = dataB[xB + yB * widthB];
-                        if (colorA.A != 0 && colorB.A != 0)
-                            return true;
-                    }
-                    posInB += stepX; // Move to next pixel in the row
-                }
-                yPosInB += stepY; // Move to the next row
-            }
-            return false; // No intersection found
+            Vector2 leftTop = new Vector2(rectangle.Left, rectangle.Top);
+            Vector2 rightTop = new Vector2(rectangle.Right, rectangle.Top);
+            Vector2 leftBottom = new Vector2(rectangle.Left, rectangle.Bottom);
+            Vector2 rightBottom = new Vector2(rectangle.Right, rectangle.Bottom);
+
+            Vector2.Transform(ref leftTop, ref transform, out leftTop);
+            Vector2.Transform(ref rightTop, ref transform, out rightTop);
+            Vector2.Transform(ref leftBottom, ref transform, out leftBottom);
+            Vector2.Transform(ref rightBottom, ref transform, out rightBottom);
+
+            Vector2 min = Vector2.Min(Vector2.Min(leftTop, rightTop), Vector2.Min(leftBottom, rightBottom));
+            Vector2 max = Vector2.Max(Vector2.Max(leftTop, rightTop), Vector2.Max(leftBottom, rightBottom));
+
+            return new Rectangle((int)min.X, (int)min.Y, (int)(max.X - min.X), (int)(max.Y - min.Y));
         }
         
+        private static bool perfectColllision(Environment a, Environment b)
+        {
+
+        }
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
